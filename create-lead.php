bff9c0932002ffff83de01ed2f3e8d3b136b3b7d
yenmangu <robert.shelford@googlemail.com>
2022-06-23 00:35:21 +0100

    Changed some legacy options

--- create-lead.php
+++ create-lead.php
@@ -1,13 +1,17 @@
 <?php
 
 $data = array(
-  "BuyerType" => $_POST["BuyerType"],
-      "applicants" => array(
-        "firstName" => $_POST["firstName"],
-        "lastName" => $_POST["lastName"],
-        "email" => $_POST["email"],
-        "telephone" => $_POST["telephone"]
-      )
+    "buyerType" => $_POST["buyerType"],
+        "applicants" => array(
+          "firstName" => $_POST["firstName"],
+          "lastName" => $_POST["lastName"],
+          "email" => $_POST["email"],
+          "telephone" => $_POST["telephone"]
+        ),
+    "organisationId" => "d75acf39-07c4-4c63-995a-e7055c58c973",
+    "introducerId" => "0",
+    "introducerBranchId" => "0",
+    "introducerNegotiatorId" => "0",
 );
 
 var_dump(json_encode($data));
@@ -16,8 +20,10 @@ $ch = curl_init();
 
 $headers = [
   "X-API-KEY: 2528e9b2-7250-48fc-9371-4c13cd5991a4",
+  "organisationId: d75acf39-07c4-4c63-995a-e7055c58c973",
   "accept: application/json",
   "content-Type: text/json",
+  "content-Length: 150"
 ];
 
 
@@ -29,12 +35,12 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
 $response = curl_exec($ch);
 
-$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$status_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
 
 $responseData = json_decode($response, true);
 
 var_dump($status_code);
-//var_dump($response);
+var_dump($response);
 
 curl_close ($ch)
 
