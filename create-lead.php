291490f0713bb90db842d66c545fc15d2275e397
yenmangu <robert.shelford@googlemail.com>
2022-06-22 21:48:02 +0100

    new array format

--- create-lead.php
+++ create-lead.php
@@ -112,19 +112,27 @@ $data= [
 ];
 
 */
+//
+// $data = array(
+//   "firstName" => $_POST("firstName"),
+//   "lastName" => $_POST("lastName"),
+//   "email" => $_POST("email"),
+//   "telephone" => $_POST("telephone")
+// );
 
 $data = array(
-  "firstName" => $_POST("firstName"),
-  "lastName" => $_POST("lastName"),
-  "email" => $_POST("email"),
-  "telephone" => $_POST("telephone")
+  "BuyerType" => $_POST["BuyerType"],
+      "applicants" => array(
+        "firstName" => $_POST["firstName"],
+        "lastName" => $_POST["lastName"],
+        "email" => $_POST["email"],
+        "telephone" => $_POST["telephone"]
+      )
 );
-
-
 
 $ch = curl_init();
 
-$header = [
+$headers = [
   "X-API-KEY: 2528e9b2-7250-48fc-9371-4c13cd5991a4",
   "accept: text/plain",
   "content-Type: application/json",
@@ -132,14 +140,12 @@ $header = [
 
 
 curl_setopt($ch, CURLOPT_URL, 'https://api.smartr365.com/api/v1/mortgage/lead/create');
-curl_setopt($ch, CURLOPT_HEADER, $header);
+curl_setopt($ch, CURLOPT_HEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
 $response = curl_exec($ch);
-
-
 
 $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
