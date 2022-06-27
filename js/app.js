f58ca6ceb42220e1cb6bfa7d8619a4db4240787f
yenmangu <robert.shelford@googlemail.com>
2022-06-27 14:51:27 +0100

    validation for the nation

--- js/app.js
+++ js/app.js
@@ -5,53 +5,26 @@ const telephoneEl = document.querySelector('#telephone');
 
 const form = document.querySelector('#apply');
 
-const isRequired = value => value === '' ? false : true;
-
-const isEmailValid = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-};
-
-const isTelephoneValid = (telephone) => {
-    const re = [0 - 9] + $;
-    return re.test(telephone);
-};
-
-const showError = (input, message) => {
-    const formField = input.parentElement;
-    formField.classList.remove('success');
-    formField.classList.add('error');
-
-    const error = formField.querySelector('small');
-    error.textContent = message;
-};
-
-const showSuccess = (input) => {
-    const formField = input.parentElement;
-
-    formField.classList.remove('error');
-    formField.classList.add('success');
-
-    const error = formField.querySelector('small');
-    error.textContent = '';
-}
-
 const checkFirstName = () => {
     const firstName = firstNameEl.value.trim();
     if (!isRequired(firstName)) {
-        showError(firstNameEl, 'First name cannot be blank.');
+        showError(firstNameEl, 'First name cannot be blank');
+    } else if (!isFirstNameValid(firstName)) {
+        showError(firstNameEl, 'First name must only contain letters');
     } else {
         showSuccess(firstNameEl);
         valid = true;
-    }
+    };
     return valid;
 };
-
 
 const checkLastName = () => {
     const lastName = lastNameEl.value.trim();
     if (!isRequired(lastName)) {
-        showError(firstNameEl, 'Last name cannot be blank');
+        showError(lastNameEl, 'Last name cannot be blank');
+    } else if (!isLastNameValid(lastName)) {
+
+        showError(lastName, 'Last name must only contain letters');
 
     } else {
         showSuccess(lastNameEl);
@@ -85,13 +58,69 @@ const checkTelephone = () => {
     return valid;
 };
 
+// const accept = () => {
+//     if (!document.querySelector('#accept').checked) {
+//         showError(accept, 'You must accept the terms to continue')
+//     } else {
+//         showSuccess(accept);
+//         valid = true;
+//     }
+//     return valid;
+// };
+
+const isFirstNameValid = (firstName) => {
+    const re = /^[a-zA-Z]+$/;
+    return re.test(firstName);
+};
+
+const isLastNameValid = (lastName) => {
+    const re = /^[a-zA-Z]+$/;
+    return re.test(lastName);
+};
+
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isTelephoneValid = (telephone) => {
+    const re = /[0 - 9] + $/;
+    return re.test(telephone);
+};
+
+const isRequired = value => value === '' ? false : true;
+
+
+const showError = (input, message) => {
+    const formField = input.parentElement;
+    formField.classList.remove('success');
+    formField.classList.add('error');
+
+    const error = formField.querySelector('small');
+    error.textContent = message;
+};
+
+const showSuccess = (input) => {
+    const formField = input.parentElement;
+
+    formField.classList.remove('error');
+    formField.classList.add('success');
+
+    const error = formField.querySelector('small');
+    error.textContent = '';
+}
+
+
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     let isFirstNameValid = checkFirstName(),
-        isLastNameValid = checkLastName()
-    isEmailValid = checkEmail()
-    isTelephoneValid = checkTelephone();
+        isLastNameValid = checkLastName(),
+        isEmailValid = checkEmail(),
+        isTelephoneValid = checkTelephone();
+
 
     let
         isFormValid = isFirstNameValid &&
